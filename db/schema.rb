1f3da79cb12c2f705039a13c27b885ec8f141011
Gu Hang <guhang92@gmail.com>
2014-09-21 22:39:05 +1000

    micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921062720) do
+ActiveRecord::Schema.define(version: 20140921123748) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
